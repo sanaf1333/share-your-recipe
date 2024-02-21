c03e793929ea639d7a8b57389b6401fdf7b6fe78
sanaf1333 <sanaf1333@gmail.com>
2024-02-21 15:52:25 +0500

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"lItSIRb4ViSgll8XBQvmcI9sj/3M1rTlqVbK+CKnSL0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"OIMkEFyE7aytIsbXf/D2sUwjIvSiv5gAfYbL7p2JRRQ=\"}"
